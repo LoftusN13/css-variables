926a74cbafaf756f389bd7b2b792ea7c8a553a89
LoftusN13 <loftus.niamh@yahoo.ie>
2022-02-04 21:31:44 +0000

    Array challenges

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -1,3 +1,4 @@
+// Array Challenge 1
 const inventors = [
     { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
     { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -93,3 +94,76 @@ const transportation = data.reduce(function(obj,item) {
     return obj;
 }, {});
 console.log(transportation);
+
+
+// Array Challenge 2
+const humans = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19?
+//const isAdult = humans.some(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//    if(currentYear - person.year >= 18) {
+//        return true;
+//    }
+//});
+
+// Shorter, cleaner version of above code
+const isAdult = humans.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+console.log(isAdult);
+
+// Array.prototype.every() // is everyone 19?
+//const allAdults = humans.every(function(person) {
+//    const currentYear = (new Date()).getFullYear();
+//    if(currentYear - person.year >= 18) {
+//       return true;
+//    }
+//});
+
+// Shorter, cleaner version of above code
+const allAdults = humans.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+console.log(allAdults);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+//const findId = comments.find(function(comment) {
+//    if(comment.id === 823423) {
+//        return true;
+//    }
+//});
+
+// Shorter, cleaner version of above code
+const findId = comments.find(comment => (comment.id === 823423));
+
+console.log(findId);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+const index = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
+// delete the comment with the ID of 823423
+
+// comments.splice(index, 1);
+// below creates new array with selected ID gone, rather than changing original
+const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+    ];
+console.log(newComments);
+console.log(comments);
