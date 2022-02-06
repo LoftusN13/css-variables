a736ba0190975d823dcc02c72db9296230fd511c
LoftusN13 <loftus.niamh@yahoo.ie>
2022-02-06 21:17:08 +0000

    Adjust size of canvas and add event listeners to mouse movements

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,6 +28,8 @@ panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 // canvas script
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 ctx.strokeStyle = '#B3FFB3';
 ctx.lineJoin = 'round';
@@ -36,3 +38,13 @@ ctx.lineCap = 'round';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+
+function draw(e) {
+    if(!isDrawing) return; //stop the funciton from running when not moused down
+    console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
